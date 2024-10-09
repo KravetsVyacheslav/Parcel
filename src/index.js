07be4bd80ef58ac1e8ff09e3761dcc2d32a374f6
Vyacheslav <0907vk@gmail.com>
2024-10-09 20:36:04 +0300

    feat: parcel

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { saveFormData } from "./formData";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("feedback-form");
+  const successMessage = document.getElementById("success-message");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = {
+      name: form.name.value,
+      email: form.email.value,
+      message: form.message.value,
+    };
+
+    saveFormData(formData);
+
+    successMessage.classList.remove("hidden");
+
+    form.reset();
+  });
+});
